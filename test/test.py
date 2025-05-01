c15bcd98a35369b6c32bfaa7aaac392d47df739d
This is Jetson speaking <test@test.com>
2025-05-02 00:17:27 +0800

    feat: add environment for conda

--- test/test.py
+++ test/test.py
@@ -13,9 +13,9 @@ while True:
     try:
         temperature = dht.temperature
         humidity = dht.humidity
-        print(dir(dht))
         # Print what we got to the REPL
-        #print("Temp: {:.1f} *C \t Humidity: {}%".format(temperature, humidity))
+        #print(dir(dht))
+        print("Temp: {:.1f} *C \t Humidity: {}%".format(temperature, humidity))
     except RuntimeError as e:
         # Reading doesn't always work! Just print error and we'll try again
         print("Reading from DHT failure: ", e.args)
